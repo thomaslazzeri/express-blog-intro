15a9ec02614c141342e123d009272f48a3b017f9
thomas <thomaslazzeri3@gmail.com>
2026-08-29 12:30:08 +0200

    Refactor server.js to improve code structure and readability

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 import express from 'express';
+import { posts } from './posts.js';
+
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
 console.log(req.path);
 next();
-});
+}); */
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Server del mio blog')
-})
+  res.send('Server del mio blog');
+});
+
+app.get('/bacheca', (req, res) => {
+  res.send('Homepage of the posts');
+});
+
+app.get('/posts', (req, res) => {
+res.json(posts);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
