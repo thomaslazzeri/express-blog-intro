ab37bda4ee6696a5a946526f541d5124201c1565
thomas <thomaslazzeri3@gmail.com>
2026-08-29 11:30:06 +0200

    Add initial posts data and images for blog posts

---
+++ posts.js
@@ -0,0 +1,32 @@
+export const posts = [
+    {
+        name: "Post 1",
+        content: "text",
+        image: "/images/post1.jpg",
+        tags: ["yellow", "text"],
+    },
+    {
+        name: "Post 2",
+        content: "text",
+        image: "/images/post2.jpg",
+        tags: ["green", "text"],
+    },
+    {
+        name: "Post 3",
+        content: "text",
+        image: "/images/post3.jpg",
+        tags: ["cian", "text"],
+    },
+    {
+        name: "Post 4",
+        content: "text",
+        image: "/images/post4.jpg",
+        tags: ["purple", "text"],
+    },
+    {
+        name: "Post 5",
+        content: "text",
+        image: "/images/post5.jpg",
+        tags: ["pink", "text"],
+    }
+];
--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+console.log(req.path);
+next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server del mio blog')
 })
 
 app.listen(port, () => {
